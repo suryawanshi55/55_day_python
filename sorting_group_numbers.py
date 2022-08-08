75efac89218b10421a648bea4672b854beb30ed4
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-08 13:09:48 +0530

    Add files via upload

---
+++ sorting_group_numbers.py
@@ -0,0 +1,13 @@
+# def num_sort(lis):
+#     lis.sort()
+#     print(lis)
+#     for i in lis : print(i,end=" ")
+# print("enter numbers:")               #3 5 7 1 3 9 8 2
+# v=[int(x)for x in input().split()]   # [1, 2, 3, 3, 5, 7, 8, 9]
+# num_sort(v)                  # 1 2 3 3 5 7 8 9 
+
+       # 2nd way
+
+# list=[int(x)for x in input ().split()]
+# list.sort(reverse=True)         # 3 6 7 8 2 1
+# print(*list)                    # 8 7 6 3 2 1
